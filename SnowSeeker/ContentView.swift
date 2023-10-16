239b0fdab6c5ebe9bde031dd4f5481ec817034f9
Alex Bonder <lexbonder@gmail.com>
2023-10-16 08:26:25 -0400

    Hacking with Swift - 100 Days of SwiftUI - Day 96

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -7,15 +7,37 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct UserView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Group {
+            Text("Name: Alex")
+            Text("Country: USA")
+            Text("Pets: Crystal")
         }
-        .padding()
+        .font(.title)
+    }
+}
+
+struct ContentView: View {
+    @State private var searchText = ""
+    let allNames = ["Subh", "Vina", "Melvin", "Stefanie"]
+    
+    var body: some View {
+        NavigationView {
+            List(filteredNames, id: \.self) { name in
+                Text(name)
+            }
+            .searchable(text: $searchText, prompt: "Look for something")
+            .navigationTitle("Searching")
+        }
+    }
+    
+    var filteredNames: [String] {
+        if searchText.isEmpty {
+            return allNames
+        } else {
+            return allNames.filter { $0.localizedCaseInsensitiveContains(searchText) }
+        }
     }
 }
 
